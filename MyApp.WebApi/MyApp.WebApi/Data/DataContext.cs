c63b104ab5e127a5d8b65a4bddda39953c5fde86
abhirampadhi <abhiraampadhi@outlook.com>
2024-07-13 03:29:56 +0200

    Add project files.

---
+++ MyApp.WebApi/MyApp.WebApi/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyApp.WebApi.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base (options) { }
+    public DbSet<Inspection> Inspections { get; set; }
+    public DbSet<InspectionType> InspectionTypes { get; set; }
+    public DbSet<Status> Statuses { get; set; }
+}
