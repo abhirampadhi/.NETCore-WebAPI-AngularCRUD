d3b552c69e40cc1650172880601c40503151e866
abhirampadhi <abhiraampadhi@outlook.com>
2024-09-02 11:25:10 +0200

    lint

--- Angular18/src/app/inspection-api.service.ts
+++ Angular18/src/app/inspection-api.service.ts
@@ -9,56 +9,56 @@ export class InspectionApiService {
 
   readonly inspectionAPIUrl = "https://localhost:7164/api";
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   // Inspection
-  getInspectionList():Observable<any[]> {
+  getInspectionList(): Observable<any[]> {
     return this.http.get<any>(this.inspectionAPIUrl + '/Inspections');
   }
 
-  addInspection(data:any) {
+  addInspection(data: any) {
     return this.http.post(this.inspectionAPIUrl + '/Inspections', data)
   }
 
-  updateInspection(id:number|string, data:any) {
+  updateInspection(id: number | string, data: any) {
     return this.http.put(this.inspectionAPIUrl + `/Inspections/${id}`, data)
   }
 
-  deleteInspection(id:number|string) {
+  deleteInspection(id: number | string) {
     return this.http.delete(this.inspectionAPIUrl + `/Inspections/${id}`)
   }
 
   //InspectionTypes  
-  getInspectionTypesList():Observable<any[]> {
+  getInspectionTypesList(): Observable<any[]> {
     return this.http.get<any>(this.inspectionAPIUrl + '/InspectionTypes');
   }
 
-  addInspectionTypes(data:any) {
+  addInspectionTypes(data: any) {
     return this.http.post(this.inspectionAPIUrl + '/InspectionTypes', data)
   }
 
-  updateInspectionTypes(id:number|string, data:any) {
+  updateInspectionTypes(id: number | string, data: any) {
     return this.http.put(this.inspectionAPIUrl + `/InspectionTypes/${id}`, data)
   }
 
-  deleteInspectionTypes(id:number|string) {
+  deleteInspectionTypes(id: number | string) {
     return this.http.delete(this.inspectionAPIUrl + `/InspectionTypes/${id}`)
   }
 
   //InspectionTypes  
-  getStatusList():Observable<any[]> {
+  getStatusList(): Observable<any[]> {
     return this.http.get<any>(this.inspectionAPIUrl + '/Status');
   }
 
-  addStatus(data:any) {
+  addStatus(data: any) {
     return this.http.post(this.inspectionAPIUrl + '/Status', data)
   }
 
-  updateStatus(id:number|string, data:any) {
+  updateStatus(id: number | string, data: any) {
     return this.http.put(this.inspectionAPIUrl + `/Status/${id}`, data)
   }
 
-  deleteStatus(id:number|string) {
+  deleteStatus(id: number | string) {
     return this.http.delete(this.inspectionAPIUrl + `/Status/${id}`)
   }
 
